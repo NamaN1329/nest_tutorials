641e5e76c0b454c09843c85cae2a144fae0f27e9
Hugh Hari <hugh.hari@rubicotech.in>
2022-04-11 23:32:28 +0530

    working on nest controller with GET and POST method

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action add a new cat';
+  }
+
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action return all the cats';
+  }
+}
