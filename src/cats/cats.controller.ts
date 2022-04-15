42fea29f44c5559483dbd2b1f130b401b37bc3a4
Hugh Hari <hugh.hari@rubicotech.in>
2022-04-15 06:14:10 +0530

    full resorce sample

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,42 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { query, Request } from 'express';
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
-    return 'This action add a new cat';
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
   }
 
   @Get()
-  findAll(@Req() request: Request): string {
+  findAll(@Query() query: ListAllEntities): string {
     return 'This action return all the cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This is the returns a #${params.id} cats`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() createCatDto: CreateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
